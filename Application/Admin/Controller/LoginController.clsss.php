fae97b8387d46f0aebf210335afcd8653cda04af
lancelotpro <lancelot.pro@outlook.com>
2017-12-04 17:36:13 +0800

    front index.html back index.html
    back login.html on the process!

---
+++ Application/Admin/Controller/LoginController.clsss.php
@@ -0,0 +1,22 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+
+
+    public function login(){
+       	$this -> display();
+    }
+
+    public function logout(){
+
+    }
+
+    public function reg(){
+
+    }
+
+    public function index(){
+    	$this -> display();
+    }
+}

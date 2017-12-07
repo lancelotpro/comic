e5a7a1d149ff6837fe2f1df021597b75403cd6f7
lancelotpro <lancelot.pro@outlook.com>
2017-12-07 09:50:17 +0800

    修复上传后图片和文字的显示问题
    上传后的数据，在入库的时候会自动转义，于是在出库时反转义即可。

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,9 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+    	$cartoon = M("Cartoon");
+    	$rows = $cartoon -> order("id desc") -> select();
+    	$this -> rows = $rows;
         $this -> display();
     }
 }
